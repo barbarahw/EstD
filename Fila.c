104975f5b4f644b7c4ed85a4873fdd4e498e4cad
barbarahw <127110674+barbarahw@users.noreply.github.com>
2023-11-27 09:07:36 -0300

    Update Fila.c

--- Fila.c
+++ Fila.c
@@ -1,101 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//struct no
-typedef struct no{
-	int dado;
-	struct no *prox;
-}no;
+typedef struct NO {
+    int dado;
+    struct NO* prox;
+    struct NO* ant;
+}NO;
 
-//struct fila
-typedef struct fila{
-	struct no *inicio;
-	struct no *fim;
-}fila;
+NO* inicio;
+NO* final;
+NO* aux;
 
-no *aux;
+void enfileirar();
 
-fila* criaFila(){
-	fila *f= (fila *) malloc(sizeof(fila));
-	if (f!=NULL){
-		f->inicio = NULL;
-		f->fim = NULL;
-	}
-	return f;
-}
+void desenfileirar();
 
-void enfileirar(fila *f){
-	no *novo = (no *)malloc(sizeof(no));
-	printf("informe o numero a ser inserido:\n");
-	scanf("%d", &novo->dado);
-	novo->prox=NULL;
-	if (f->inicio==NULL){
-        f->fim=novo;
-        f->inicio=novo;
-	}
-	else {
-        f->fim->prox=novo;
-        f->fim=novo;
-	}
-	printf("Numero inserido na fila!\n");
-}
-
-void desenfileirar(fila *f) {
-    if (f->fim==NULL) {
-        printf("Fila vazia!\n");
-    } else {
-        aux=f->inicio;
-        printf("%d removido!\n", f->inicio->dado);
-        f->inicio=f->inicio->prox;
-        free(aux);
-    }
-}
-
-void consultarFila(fila *f){
-    if (f->fim==NULL){
-        printf("Fila vazia!\n");
-    }
-    else {
-        aux=f->inicio;
-        do {
-            printf(" %d ", aux->dado);
-            aux=aux->prox;
-        }while(aux!=NULL);
-        printf("\n");
-    }
-}
-
-void esvaziarFila(fila *f) {
-    if (f->fim==NULL){
-        printf("Fila vazia!\n");
-    }
-    else {
-        aux = f->inicio;
-        do{
-            f->inicio=f->inicio->prox;
-            free(aux);
-            aux=f->inicio;
-        }while(aux!=NULL);
-        printf("Pilha esvaziada!\n");
-    }
-}
-
+void imprimirFila();
 
 int main() {
-    int escolha;
-    fila *f = criaFila();
-
-    do {
-        printf("1-Enfileirar\n2-Desenfileirar\n3-Consultar fila\n4-Esvaziar fila\n0-Sair\n");
-        scanf("%d", &escolha);
-        switch (escolha){
-            case 1: enfileirar(f); break;
-            case 2: desenfileirar(f); break;
-            case 3: consultarFila(f); break;
-            case 4: esvaziarFila(f); break;
-        }
-    } while (escolha!=0);
-
-
+    inicio = NULL;
+    final = NULL;
+        
+    enfileirar();
+    enfileirar();
+    enfileirar();
+    enfileirar();
+    imprimirFila();
+    
     return 0;
+}
+
+
+void enfileirar(){
+    NO* novo = malloc(sizeof(NO));
+    printf("Informe o novo dado: ");
+    scanf("%d", &novo->dado);
+    if (inicio == NULL){
+        inicio = novo;
+        novo->prox = NULL;
+        novo->ant = NULL;
+    }
+    else{
+        novo->ant = final;
+        final->prox = novo;
+        novo->prox = NULL;
+    }
+    final = novo;
+    
+    printf("O elemento foi inserido!\n");
+}
+
+void desenfileirar(){
+    if (inicio == NULL)
+        printf("A fila está vazia!\n");
+    else{
+        aux = inicio;
+        printf("O elemento %d foi removido\n", aux->dado);
+        inicio = inicio->prox;
+        free(aux);
+
+        if (inicio == NULL) {
+            final = NULL;
+        }
+    }
+}
+
+void imprimirFila() {
+    if (inicio == NULL)
+        printf("Fila vazia!");
+    else {
+        aux = inicio;
+        printf("Fila: ");
+        while(aux != NULL){
+            printf("%d\t", aux->dado);
+            aux=aux->prox;
+        }
+    }
 }
